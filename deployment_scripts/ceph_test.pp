1791da7e67baaffb43c965488ad7fcfeefc3eb68
root <root@nailgun.test.domain.local>
2016-04-27 18:03:09 +0000

    new

--- deployment_scripts/ceph_test.pp
+++ deployment_scripts/ceph_test.pp
@@ -1,10 +1,10 @@
 notice('MODULAR: ceph_test.pp')
-$plugin_yaml = "/etc/hiera/plugins/ceph_test/ceph.yaml"
-file { "/etc/hiera/plugins/ceph_test":
+$plugin_yaml = "/etc/hiera/ceph.yaml"
+file { "/etc/hiera":
 	ensure => directory,
 	}
 file { "$plugin_yaml":
         ensure => file,
         content => template('/etc/fuel/plugins/ceph_test-1.0/ceph.erb'),
-        require => File["/etc/hiera/plugins/ceph_test"],
+	require => File["/etc/hiera"],
 	}
