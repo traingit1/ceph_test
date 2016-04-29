0ee806e2259895f4fc89ff38baaa6437d14ab2d9
root <root@nailgun.test.domain.local>
2016-04-29 13:52:16 +0000

    change location in hiera folder

--- deployment_scripts/ceph_test.pp
+++ deployment_scripts/ceph_test.pp
@@ -1,10 +1,14 @@
 notice('MODULAR: ceph_test.pp')
-$plugin_yaml = "/etc/hiera/ceph.yaml"
-file { "/etc/hiera":
+$plugin_yaml = "/etc/hiera/plugins/ceph_test.yaml"
+file { "/etc/hiera/":
 	ensure => directory,
+	}
+file { "/etc/hiera/plugins/":
+	ensure => directory,
+	require => File["/etc/hiera"],
 	}
 file { "$plugin_yaml":
         ensure => file,
         content => template('/etc/fuel/plugins/ceph_test-1.0/ceph.erb'),
-	require => File["/etc/hiera"],
+        require => File["/etc/hiera/plugins/"],
 	}
